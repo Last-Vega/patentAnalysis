01c405f014418d7dc307138cd6e7e14cfbbd1e4f
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2022-01-23 01:50:53 +0900

    Update args.py

--- backend/model/args.py
+++ backend/model/args.py
@@ -3,16 +3,16 @@
 hidden1_dim:int = 4
 hidden2_dim:int = 2
 use_feature:bool = True
-num_epoch:int = 10
+num_epoch:int = 200
 learning_rate:float = 0.01
 
 # num_patent:int = 44564
 # num_company:int = 4530
 # num_term:int = 9960
 
-num_patent:int = 2682
-num_createdBy:int = 621
-num_term:int = 7205
-num_headIPC:int = 148
-num_f_term:int = 8291
-num_createdAt:int = 21
+# num_patent:int = 2682
+# num_createdBy:int = 621
+# num_term:int = 7205
+# num_headIPC:int = 148
+# num_f_term:int = 8291
+# num_createdAt:int = 21
