5b6bb3798e3475f1bfd21dc59ba6f67d88faa3d0
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2022-03-06 20:15:20 +0900

    Update input_data.py

--- backend/model/input_data.py
+++ backend/model/input_data.py
@@ -20,26 +20,26 @@ def createFeatures(adj:csr_matrix) -> lil_matrix:
     return features
 
 
-# f_name:str = './W_adj.adj'
+f_name:str = './W_adj.adj'
 
-# f_name = f'{temp_folder}/W_adj0122.adj'
-# adj:csr_matrix = loadBinary(f_name)
+f_name = f'{temp_folder}/W_adj0122.adj'
+adj:csr_matrix = loadBinary(f_name)
 
-# features:lil_matrix = createFeatures(adj)
+features:lil_matrix = createFeatures(adj)
 
-# f_name:str = f'{temp_folder}/W_bi0122.adj'
-# bi_adj:csr_matrix = loadBinary(f_name)
+f_name:str = f'{temp_folder}/W_bi0122.adj'
+bi_adj:csr_matrix = loadBinary(f_name)
 
-# f_name = f'{temp_folder}/adj0122.dict'
-# adj_dict = loadBinary(f_name)
-# f_name = f'{temp_folder}/bi0122-1.dict'
-# bi_dict = loadBinary(f_name)
-
-f_name = f'{temp_folder}/adj0304.dict'
+f_name = f'{temp_folder}/adj0122.dict'
 adj_dict = loadBinary(f_name)
-f_name = f'{temp_folder}/bi0304.dict'
+f_name = f'{temp_folder}/bi0122-1.dict'
 bi_dict = loadBinary(f_name)
-features:lil_matrix = createFeatures(adj_dict['CPC'])
+
+# f_name = f'{temp_folder}/adj0304.dict'
+# adj_dict = loadBinary(f_name)
+# f_name = f'{temp_folder}/bi0304.dict'
+# bi_dict = loadBinary(f_name)
+# features:lil_matrix = createFeatures(adj_dict['CPC'])
 
 # with open('../vars/annotate.ct', 'rb') as rb:
 #     annotate_list:list = pickle.load(rb)
