bc5311543b75d66fc157e7dfb9c71f2eaf74c355
Last-Vega <watanabe.shingo.ss@almni.tsukuba.ac.jp>
2022-07-11 14:12:58 +0900

    modify for gpu

--- backend/model/train.py
+++ backend/model/train.py
@@ -4,10 +4,17 @@ from scipy.sparse import csr_matrix, lil_matrix
 from .args import *
 import os
 from .util import fix_seed, prepare_adj_for_training, prepare_features_for_training, model_init
+import cupy as cp
 
 # from input_data import adj, features, bi_adj
 from .. import app
 temp_folder = app.config['TEMP_FOLDER']
+
+torch.set_default_tensor_type('torch.cuda.FloatTensor')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# print(device)
+pool = cp.cuda.MemoryPool(cp.cuda.malloc_managed)
+cp.cuda.set_allocator(pool.malloc)
 
 def weighting(dict, list, size):
     weighted_adj = torch.zeros((size[0],size[1]))
@@ -33,11 +40,11 @@ def initialTrain():
     adj = weighting(adj_dict, adj_weight_list, adj_shape)
     bi_adj = weighting(bi_dict, bi_weight_list, bi_shape)
 
-    weight_tensor, adj_norm, norm, adj_label, adj_orig, test_edges, test_edges_false = prepare_adj_for_training(adj)
+    weight_tensor, adj_norm, norm, adj_label, adj_orig = prepare_adj_for_training(adj)
     features = prepare_features_for_training(features)
     graph_dim = features.shape[1]
 
-    bi_weight_tensor, bi_adj_norm, bi_norm, bi_adj_label, bi_adj_orig, bi_test_edges, bi_test_edges_false = prepare_adj_for_training(bi_adj)
+    bi_weight_tensor, bi_adj_norm, bi_norm, bi_adj_label, bi_adj_orig = prepare_adj_for_training(bi_adj)
     bipartite_dim = bi_adj.shape[1]
 
 
@@ -45,10 +52,15 @@ def initialTrain():
     print('ok')
     for epoch in range(num_epoch):
         A_pred, Bi_pred = model(features, bi_adj_norm)
+        A_pred = A_pred.to('cpu')
+        Bi_pred = Bi_pred.to('cpu')
         optimizer.zero_grad()
+        # loss = norm*F.binary_cross_entropy(A_pred.view(-1), adj_label.to_dense().view(-1), weight = weight_tensor) + bi_norm*F.binary_cross_entropy(Bi_pred.view(-1), bi_adj_label.to_dense().view(-1), weight = bi_weight_tensor)
+        # kl_divergence1 = 0.5/ A_pred.size(0) * (1 + 2*model.logstd - model.mean**2 - torch.exp(model.logstd)**2).sum(1).mean()
+        # kl_divergence2 = 0.5/ Bi_pred.size(0) * (1 + 2*model.siguma - model.mu**2 - model.siguma**2).sum(1).mean()
         loss = norm*F.binary_cross_entropy(A_pred.view(-1), adj_label.to_dense().view(-1), weight = weight_tensor) + bi_norm*F.binary_cross_entropy(Bi_pred.view(-1), bi_adj_label.to_dense().view(-1), weight = bi_weight_tensor)
-        kl_divergence1 = 0.5/ A_pred.size(0) * (1 + 2*model.logstd - model.mean**2 - torch.exp(model.logstd)**2).sum(1).mean()
-        kl_divergence2 = 0.5/ Bi_pred.size(0) * (1 + 2*model.siguma - model.mu**2 - model.siguma**2).sum(1).mean()
+        kl_divergence1 = 0.5/ A_pred.size(0) * (1 + 2*model.logstd.to('cpu') - model.mean.to('cpu')**2 - torch.exp(model.logstd.to('cpu'))**2).sum(1).mean()
+        kl_divergence2 = 0.5/ Bi_pred.size(0) * (1 + 2*model.siguma.to('cpu') - model.mu.to('cpu')**2 - model.siguma.to('cpu')**2).sum(1).mean()
         loss -= kl_divergence1
         loss -= kl_divergence2
         loss.backward()
