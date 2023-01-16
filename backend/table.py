77ffe8eb2c4c72a13a5c07bd5926a154f2555305
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2023-01-16 23:50:12 +0900

    仮のテーブル作成

--- backend/table.py
+++ backend/table.py
@@ -1,5 +1,6 @@
 from . import app
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.mysql import TIMESTAMP as Timestamp
 
 db = SQLAlchemy(app)
 
@@ -27,3 +28,59 @@ class Test(db.Model):
 
     def __repr__(self):
         return '<Test %r, %r, %r>' % self.id, self.text, self.number
+
+# モデルのパラメータのファイル名を保存する
+class Model_file(db.Model):
+    __tablename__ = 'modelfiles'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    f_name = db.Column(db.String(1000))
+    timestamp = db.Column(Timestamp)
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            text=self.f_name,
+            number=self.timestamp
+        )
+
+    def __repr__(self):
+        return '<model %r>' % self.id
+
+
+# meta-pathの重みのファイル名を保存する
+class Metapath_file(db.Model):
+    __tablename__ = 'metapathfiles'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    f_name = db.Column(db.String(1000))
+    timestamp = db.Column(Timestamp)
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            text=self.f_name,
+            number=self.timestamp
+        )
+
+    def __repr__(self):
+        return '<metapath %r, %r, %r>' % self.id
+
+
+# 潜在表現のファイル名を保存する
+class Latent_file(db.Model):
+    __tablename__ = 'latentfiles'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    f_name = db.Column(db.String(1000))
+    timestamp = db.Column(Timestamp)
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            text=self.f_name,
+            number=self.timestamp
+        )
+
+    def __repr__(self):
+        return '<metapath %r, %r, %r>' % self.id
